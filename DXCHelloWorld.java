b8465f2fd50928587ffef1150e2768f6b7caf776
svk1241 <69514148+svk1241@users.noreply.github.com>
2022-05-30 18:43:00 +0530

    Update DXCHelloWorld.java
    commit

--- DXCHelloWorld.java
+++ DXCHelloWorld.java
@@ -2,6 +2,6 @@ public class DXCHelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello world from DXC Private Repo");
+    System.out.println("Hello world from DXC Private repo 2");
   }
 }
